6b6c26aa38b37e40f5f42152c5c325089757f5cc
João Correia <jpcorreia@mail.com>
2022-05-16 07:45:37 +0100

    1

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 // const { auth } = require('express-oauth2-jwt-bearer');
-const jwt = require('express-jwt').jwt;
+// const jwt = require('express-jwt').jwt;
 const jwksRsa = require('jwks-rsa');
-// const { expressjwt: jwt } = require("express-jwt");
+const { expressjwt: jwt } = require("express-jwt");
 
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
