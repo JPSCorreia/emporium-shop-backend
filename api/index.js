45a175cd56dfea69077a518d75da340cd52c412b
João Correia <jpcorreia@mail.com>
2023-10-09 12:30:26 +0100

    1

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 import app from '../server';
 
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(``);
+});
+
+
 export default app
